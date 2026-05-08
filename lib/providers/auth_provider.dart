9d14cee6cc4172c0bbf06700d157c8c36e6d7121
santiagobernal123 <santiagobarevalo05@gmail.com>
2026-05-07 23:54:51 -0500

    feat: agregar protocolo de autodestruccion

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:vibration/vibration.dart';
 
 class AuthProvider extends ChangeNotifier {
   final LocalAuthentication auth = LocalAuthentication();
 
   bool _isAuthenticated = false;
+  int _failedAttempts = 0;
+  bool _isLocked = false;
+  String _statusMessage = "";
 
   bool get isAuthenticated => _isAuthenticated;
+  bool get isLocked => _isLocked;
+  String get statusMessage => _statusMessage;
 
   Future<void> authenticate() async {
+    if (_isLocked) return;
+
     try {
       bool canCheck = await auth.canCheckBiometrics;
       bool isSupported = await auth.isDeviceSupported();
@@ -28,11 +36,41 @@ class AuthProvider extends ChangeNotifier {
 
       if (didAuthenticate) {
         _isAuthenticated = true;
+        _failedAttempts = 0;
+        _statusMessage = "";
+      } else {
+        _failedAttempts++;
+
+        if (_failedAttempts >= 3) {
+          await _triggerSelfDestruct();
+        }
       }
 
       notifyListeners();
     } catch (e) {
       debugPrint("ERROR BIOMETRIA: $e");
     }
+  }
+
+  Future<void> _triggerSelfDestruct() async {
+    _isAuthenticated = false;
+    _isLocked = true;
+
+    _statusMessage =
+        "⚠️ Protocolo de Autodestrucción Iniciado\nBloqueado 5 segundos";
+
+    if (await Vibration.hasVibrator() ?? false) {
+      Vibration.vibrate(duration: 5000, amplitude: 255);
+    }
+
+    notifyListeners();
+
+    await Future.delayed(const Duration(seconds: 5));
+
+    _failedAttempts = 0;
+    _isLocked = false;
+    _statusMessage = "✅ Puedes intentar nuevamente";
+
+    notifyListeners();
   }
 }
